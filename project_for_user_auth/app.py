43f87b7d3b5b6962442899eceb01fe14995d56cc
Divante Parness <divante111@gmail.com>
2023-08-21 21:15:12 -0500

    worked on the user authentication

---
+++ project_for_user_auth/app.py
@@ -0,0 +1,89 @@
+from myproject import app, db
+
+from flask import render_template, redirect, request, url_for, flash, abort
+
+from flask_login import login_user, login_required, logout_user
+from myproject.models import User
+from myproject.forms import LoginForm, RegistrationForm
+from werkzeug.security import generate_password_hash, check_password_hash
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+##name of the page as usual but the second line sends this function through
+##a funciton that requires the user to be logged in to view the page
+@app.route('/welcome')
+@login_required
+def welcome_user():
+    return render_template('welcome_user.html')
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have logged out!')
+    return redirect(url_for('home'))
+
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    
+    form  = LoginForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(email= form.email.data).first()
+        if user.check_password(form.password.data) and user is not None:
+            login_user(user)
+            flash('Logged in successfully')
+
+            #flask saves the request in next
+            #so we are grabbing the request from next
+            next = request.args.get('next')
+
+
+            # if next exists or is not equal to /
+            if next == None or not next[0] =='/':
+                next = url_for('welcome_user')
+            #redirects the user to the page/s they are allowed to visit after
+            #they have logged in
+            return redirect(next)
+        
+    #if the user fails to loging correctly they are rerouted to the home page
+    return render_template('login.html', form = form)
+
+## get and post tells the method it is allowed to retrieve and post data
+## to and from the server
+@app.route('/register', methods = ['GET', 'POST'])
+def register():
+    ##takes the registration form from myproject.forms method
+    form = RegistrationForm()
+
+
+    #checks to see if the form has been submitted and the submitted data 
+    #passes the validation rules defined in the forms fild's
+    if form.validate_on_submit():
+        print('/n'*5)
+        print(form.email.data)
+        user = User(email=form.email.data,
+                    username=form.username.data, 
+                    password=form.password.data)
+        
+        #adding a record to the SQLAlchemy database
+        #and commiting the information
+        
+        db.session.add(user)
+        db.session.commit()
+
+
+        #flashes a message to the screen for the user to see
+        flash('Thanks for registration!')
+        #name of the html file
+        return redirect(url_for('login'))
+    
+    return render_template('register.html', form = form)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+        
